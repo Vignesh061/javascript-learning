60e839b7562b6cac7d16aa9a21f7480863d7b8e0
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-23 21:45:52 +0530

    Implemented Problem

---
+++ 01.Day/03.Exercise/01.Problem.js
@@ -0,0 +1,22 @@
+// JavaScript Variables
+
+// Exercise 1
+// Problem 1
+
+// 1. Create a variable named "Price" and set it to a specific price value.
+
+// 2. Create a variable named "Product" and set it to a product name.
+
+// 3. Create a variable named "Tax" and set it to a specific tax value.
+
+// 4. Print the name of the product.
+
+// 5. Calculate the total price amount, including tax (total = Price + Tax), and then print it.
+
+
+var price = 450
+var product = "Laptop"
+var tax = 50
+var total = price + tax
+console.log(product)
+console.log(total)
