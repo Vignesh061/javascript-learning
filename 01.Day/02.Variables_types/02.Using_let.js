87b0cb99f5c632edb741f69b399564086e886bae
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-23 21:39:37 +0530

    Let keyword usage

---
+++ 01.Day/02.Variables_types/02.Using_let.js
@@ -0,0 +1,14 @@
+
+// let keyword is block scope
+console.log("Here we using a let keyword")
+// {
+//     let b=6
+
+// }
+// console.log(b)
+//error will occur
+
+{
+    let a=5
+    console.log(a)
+}
