3ae98c80599e743a4489372efba46300d03e36e8
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-23 21:46:33 +0530

    Implemented Problem

---
+++ 01.Day/03.Exercise/02.Problem.js
@@ -0,0 +1,25 @@
+//  JavaScript Variables
+
+// Exercise
+
+// Problem 2
+
+// 1. Declare a variable named "FruitName."
+
+// 2. Declare a variable named "count."
+
+// 3. Declare a variable named "Price" and set it to the price of the fruit.
+
+// 4. Declare a variable named "total" and calculate it by multiplying the "count" by the "Price."
+
+// 5. Print the name of the fruit.
+
+// 6. Print the total price of the fruits.
+
+
+var Fruitname = "apple"
+var count = 5 
+var price = 10
+var total = count * price
+console.log(Fruitname)
+console.log(total)
