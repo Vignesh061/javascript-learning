f990071ed3621c19f8666933df53bf4810d26af6
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-23 21:41:03 +0530

    const keyword usage

---
+++ 01.Day/02.Variables_types/03.Using_const.js
@@ -0,0 +1,15 @@
+// Here we using a const variable
+// in javascript , the const keyword is used to declare a constant varible .
+// In a constanat variable ,once we assigned a value , we cannot reassigned or redecleared .
+// It provides a way to create a variables that are meant to be immutanble
+  
+
+// const a = 19 //Assignment to constant variable
+// a = 6
+// console.log(a)
+
+// so error will be ocure
+
+// Using const keyword : block scope
+const b=4
+console.log(b)
