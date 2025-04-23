bff5d2e3d3f01e5929e392f1a18e578f38f2901f
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-23 21:38:36 +0530

    Var keyword usage

---
+++ 01.Day/02.Variables_types/01.Using_var.js
@@ -0,0 +1,14 @@
+// here we using var keywored to declear a variable
+
+// using var keyword - function scope or global scope
+var a=5;
+console.log(" Here we using var keyword:",a)
+
+// Using var keyword
+console.log("Here we using  a  var key word")
+{
+    var b = 3
+}
+ 
+console.log(b)
+// error will not occur
