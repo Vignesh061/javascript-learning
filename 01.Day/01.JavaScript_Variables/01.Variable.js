2bf6e19aee03e5af12f8d9b1bda09b7366f8f360
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-23 21:31:04 +0530

    Learned Variables

---
+++ 01.Day/01.JavaScript_Variables/01.Variable.js
@@ -0,0 +1,30 @@
+// javaScript Variables 
+
+// variables are containers for storing information or Dats 
+
+var a=10;
+var b=30;
+console.log("The some of two number is ",a+b);
+
+// in the above program a and b are variables , 10 and 20 are Integer Data
+
+// javascript variables can be declear into 3 ways 
+// 01.Using var
+// 02.Using let
+// 03.Using cost
+
+// using var keyword - function scope or global scope
+var a="Hello buddy";
+console.log(a)
+
+
+// using let
+let b="Hello"
+console.log(b)
+
+
+//using const
+let c="world"
+console.log(c)
+
+
