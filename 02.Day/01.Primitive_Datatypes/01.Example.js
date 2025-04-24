83ebd4c27c6f493c02ac45502a6dc1fa94b96be8
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-24 22:44:59 +0530

    Implemented Problem in Primitive Datatypes

---
+++ 02.Day/01.Primitive_Datatypes/01.Example.js
@@ -0,0 +1,32 @@
+//  In a JavaScript The Datatype is Two types
+
+// 01 .Primitive Data types
+
+
+// 01.Number
+var num = 10
+console.log(typeof(num))
+
+
+// 02.String
+
+var str = "How are you"
+console.log(typeof(str))
+
+// 03.Boolean
+
+var bool= true  
+console.log(typeof(bool))
+
+// 04.Null
+
+var a = null
+console.log(typeof(a))
+
+// 05.Undefined
+
+var ud 
+console.log(typeof(ud))
+
+
+ 
