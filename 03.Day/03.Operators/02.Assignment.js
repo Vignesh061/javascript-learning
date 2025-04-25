392dab4b053b6fc7860f194caeab89ae5682fec0
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-25 20:20:10 +0530

    Implemented Problem In Assignment Operator

---
+++ 03.Day/03.Operators/02.Assignment.js
@@ -0,0 +1,7 @@
+// 2. Assignment Operators
+console.log("\nAssignment Operators:");
+let x = 5;
+x += 2;  // Same as x = x + 2
+console.log("Add and assign:", x); // 7
+x *= 3;  // Same as x = x * 3
+console.log("Multiply and assign:", x); // 21
