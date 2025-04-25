3dc669f412ef45e527cad5dc617e033d81468d80
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-25 20:18:51 +0530

    Implemented Problem In Arithmetic Operator

---
+++ 03.Day/03.Operators/01.Arithmetic.js
@@ -0,0 +1,9 @@
+// 1. Arithmetic Operators
+console.log("Arithmetic Operators:");
+let a = 10, b = 3;
+console.log("Addition:", a + b);         // 13
+console.log("Subtraction:", a - b);      // 7
+console.log("Multiplication:", a * b);   // 30
+console.log("Division:", a / b);         // 3.3333
+console.log("Modulus:", a % b);          // 1 reminder
+console.log("Exponentiation:", a ** b);  // 1000
