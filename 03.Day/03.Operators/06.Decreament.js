e5252b063f2a575b350810dbeb9dbf560c5b8bd2
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-25 20:25:29 +0530

    Implemented problem In Decreament Operator

---
+++ 03.Day/03.Operators/06.Decreament.js
@@ -0,0 +1,28 @@
+// post decreament 
+a= 10
+a--
+console.log(a)
+ 
+// If we using a post decreament (Variable--), While we using a 2rd variable It wil not decreament the variable
+
+b=10
+c=b--
+console.log(c) // op:11
+
+
+
+//Pre Decreament
+
+e=10
+--e
+console.log(e)
+ 
+
+//It will decrease value because we usig Pre decreament
+
+f=10
+g=--f
+console.log(g)
+
+
+
