8ea63b7cccbeb1c27b811618678d50af0e6f9ce9
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-25 20:22:45 +0530

    Implemented Problem in Logical Operator

---
+++ 03.Day/03.Operators/04.Logical.js
@@ -0,0 +1,11 @@
+// 4. Logical Operators
+console.log("\nLogical Operators:");
+let isRaining = true, isCold = false;
+console.log("Logical AND:", isRaining && isCold); // false
+console.log("Logical OR:", isRaining || isCold); // true
+console.log("Logical NOT:", !isRaining);         // false
+
+// Summary of Logical Operators
+// && (AND): Returns true if both conditions are true.
+// || (OR): Returns true if at least one condition is true.
+// ! (NOT): Reverses the boolean value (true becomes false and vice versa).
