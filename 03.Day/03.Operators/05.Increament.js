7c0a434f2eee41025a94c35f7892bb47a53c7e07
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-25 20:24:27 +0530

    Implemented Problem In Increament Operator

---
+++ 03.Day/03.Operators/05.Increament.js
@@ -0,0 +1,30 @@
+
+
+// Post increament
+var b=10
+b++
+console.log(b)
+
+
+
+
+// If we using a Post increament (variable++) ,While using 2rd variable it will not increament the value
+var c=10
+d=c++
+console.log(d) // op:12
+
+
+//pre increament
+
+var a=10
+++a
+console.log(a)
+
+// while we using a 2rd variable ,we could use pre incraement (++variable)  then only the we can increment the value
+
+var e = 10
+var f = ++e
+console.log(f)
+
+
+
