cceee3d2c5f53d3bc81442d2b23a7fe9bd577607
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-26 19:55:39 +0530

    Created Function_Syntax

---
+++ 04.Day/01.Function_Syntax/Syntax.js
@@ -0,0 +1,12 @@
+//JavaScript Functions 
+
+//A JavaScript functions is a block of code design to perform a particular task
+//A JavaScript functions is executed when "SOMETHING" involes it (call it)
+
+
+//Syntex:
+function funname(){
+ console.log("Hey there")   
+
+}
+funname()
