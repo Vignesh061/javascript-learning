acf4c8d6caf9bf35c95d9bb97817a77a7be14f42
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-26 20:06:19 +0530

    Created Return Type Syntax

---
+++ 04.Day/03.Return_types/Syntax.js
@@ -0,0 +1,15 @@
+//Return Type
+
+// The return keyword is used within a function to specify the value the function should produce as it result or return to the caller.
+// When a function is executed and encounters a return statemts , it immediately stops executing and return the specified value to the calling code .
+//  Here show the return keyword works
+
+//Syntax
+
+function fun()
+{
+    return "Hey there"
+}
+
+var a = fun()
+console.log(a)
