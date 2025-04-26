477c2725323bcaed577c242256d3c86b4dbeaaff
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-26 20:04:10 +0530

    Implemented Problem Using Parameters

---
+++ 04.Day/02.Parameters/01.Exercise/01.Problem.js
@@ -0,0 +1,14 @@
+//Question 1
+
+//-Create a function called area()
+//-This function should get 2 values as parameters (length ,breath)
+//-Pass two values to the fuctions and print the result
+
+
+function area( length , breath)
+{
+    console.log("Area is "+length*breath)
+
+}
+
+area(8,2)
