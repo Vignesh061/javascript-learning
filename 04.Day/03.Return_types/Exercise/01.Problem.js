243c824f2846fef49b8092329666ad9c43844921
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-26 20:10:13 +0530

    Implemented Problem Using Return Type

---
+++ 04.Day/03.Return_types/Exercise/01.Problem.js
@@ -0,0 +1,12 @@
+/**
+ * Question: Write a function called calc that takes two numbers as parameters and performs addition. 
+ * The function should return the result using the return keyword. 
+ * Provide an example of how to call this function and display the result.
+ */
+
+function calc(a,b)
+{
+    return a+b
+}
+var a = calc(45,10)
+console.log(a)
---
+++ 04.Day/03.Return_types/Exercise/02.Problem.js
@@ -0,0 +1,13 @@
+/**
+ * Question: Write a function called `subtract` that takes two numbers as parameters and performs subtraction. 
+ * The function should return the result using the return keyword. 
+ * Provide an example of how to call this function and display the result.
+ */
+
+
+function subtract(a,b)
+{
+    return a-b
+}
+var sub=subtract(7,6)
+console.log(sub)
