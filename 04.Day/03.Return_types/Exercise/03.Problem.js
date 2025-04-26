dc68d4955d9b68d57bdc55497116b65de4f9d7c1
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-26 20:07:10 +0530

    Implemented Problem Using

---
+++ 04.Day/03.Return_types/Exercise/03.Problem.js
@@ -0,0 +1,13 @@
+/**
+ * Question: Write a function called `multiply` that takes two numbers as parameters and performs multiplication. 
+ * The function should return the result using the return keyword. 
+ * Provide an example of how to call this function and display the result.
+ */
+
+
+function multiply(a,b)
+{
+    return a*b
+}
+var mul=multiply(5,4)
+console.log(mul)
