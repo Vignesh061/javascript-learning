9c781c198dbd16834eec736ce023dea9989b0598
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-26 20:03:05 +0530

    Created Parameter Syntax

---
+++ 04.Day/02.Parameters/01.Parameters_Syntax.js
@@ -0,0 +1,11 @@
+//Parameters 
+
+// In JavaScript the terms "Parameters " typically refers to variables and placeholder that you define when declearing a function.
+// These Parameters serve as inputs to function and allow you to pass data into the function when you call it
+
+function add(a,b)
+{
+    console.log(a+b)
+
+}
+add(20,10)
