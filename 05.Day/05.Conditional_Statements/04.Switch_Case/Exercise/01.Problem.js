2a31b05581a29587b06a99ce97351c6e213c7cf8
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-27 22:01:07 +0530

    Implemented Problem Using Switch Case

---
+++ 05.Day/05.Conditional_Statements/04.Switch_Case/Exercise/01.Problem.js
@@ -0,0 +1,31 @@
+let day="hello";
+
+switch(day)
+{
+    case 1:
+        console.log(" It is Monday");
+        break;
+
+    case 2:
+        console.log("It is Tuesday");
+        break;
+    case 3:
+        console.log("It is wednesday");
+        break;
+    case 4:
+        console.log("It is Thrusday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7:
+        console.log("It is Sunday");
+        break;
+
+    default:
+        console.log(`${day} is not a day`);
+
+}
---
+++ 05.Day/05.Conditional_Statements/04.Switch_Case/Exercise/02.Problem.js
@@ -0,0 +1,27 @@
+let score="jr;";
+let grade;
+ switch(true)
+ {
+    case score>=90:
+        console.log("A Grade");
+        break;
+    case score>=80:
+        console.log("B Grade");
+        break;
+    case score>=70:
+        console.log("C Grade");
+        break;
+    case score>=60:
+        console.log("D Grade");
+        break;
+
+    case score>=50:
+        console.log("E Grade");
+        break
+
+    case score>=40:
+        console.log("You are Fail");
+        break;
+    default:
+        console.log("Enter your score");
+ }
