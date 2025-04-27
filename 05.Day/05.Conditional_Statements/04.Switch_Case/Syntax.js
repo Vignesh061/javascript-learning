3ab8ee0e245c9e07b40c92e4ef2b411f6994543e
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-27 22:02:26 +0530

    Switch Case Syntax

---
+++ 05.Day/05.Conditional_Statements/04.Switch_Case/Syntax.js
@@ -0,0 +1,14 @@
+//Switch = can be and efficient replacement to many else if statements
+
+/*switch (expression) {
+    case value1:
+      // Code to execute if expression === value1
+      break;
+    case value2:
+      // Code to execute if expression === value2
+      break;
+    // Add more cases as needed
+    default:
+      // Code to execute if no case matches
+  }
+  */
