314d1a9c6e514fd989e611ff21cafc8d4a166d04
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-27 21:59:55 +0530

    Else If Syntax

---
+++ 05.Day/05.Conditional_Statements/03.else_if/Syntax.js
@@ -0,0 +1,13 @@
+/*
+if (condition)
+{
+code to be executed if the condition is true
+}
+else if(condition)
+{
+ code to be executed if the condition is true
+}
+else{
+ code to be executed if the condition is true
+}
+*/
