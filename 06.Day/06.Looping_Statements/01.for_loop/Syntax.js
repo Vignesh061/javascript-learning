8e65e877b4aa9df28aa939b3d201891cc56e4dc9
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-28 23:23:39 +0530

    For Loop Syntax

---
+++ 06.Day/06.Looping_Statements/01.for_loop/Syntax.js
@@ -0,0 +1,25 @@
+/**
+ * For loop 
+ * In a javaScript , a for loop is a control flow statements 
+ * used to repeatedly executed a block of code based on a specification condition .The general syntax of a for loop in javaScript is as floows:
+ * 
+ * 
+ * for (initilization ; condition ; iteration)
+ * {
+ * code to be executed in each iteration
+ * }
+ * 
+ * 
+ * 
+ * initilization:eating 1(eg:starting of the eating times)
+ * condition : eating 5 (no of the eating times 5)
+ * iteration:count=eating=eating+ 1;
+ * 
+ */
+
+//example
+
+for(eating=1;eating<=5;eating=eating+1)
+{
+    console.log("eating")
+}
