2246520a8c7f1f58df0c9de5ea9fbb76cc9f7d6e
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-28 23:28:05 +0530

    While Loop Syntax

---
+++ 06.Day/06.Looping_Statements/02.While_loop/Syntax.js
@@ -0,0 +1,18 @@
+/**
+ * While loop
+ * In JavaScript, a while loop is a control flow statement
+ * used to repeatedly execute a block of code as long as the specified condition evaluates to true.
+ * The general syntax of a while loop in JavaScript is as follows:
+ * (initilization)
+ * while (condition) {
+ *   code to be executed as long as the condition is true
+ *  (iteration)
+ * }
+ */
+
+var i=1;
+while(i<=5)
+{
+    console.log("Hello")
+    i++
+}
