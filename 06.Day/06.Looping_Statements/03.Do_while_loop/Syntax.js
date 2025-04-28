c28f2b8baf726683d0eab854e365abd457ae0975
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-28 23:36:45 +0530

    Do Whlie Loop Syntax

---
+++ 06.Day/06.Looping_Statements/03.Do_while_loop/Syntax.js
@@ -0,0 +1,17 @@
+/**
+ * Do...while loop
+ * In JavaScript, a do...while loop is a control flow statement
+ * used to execute a block of code at least once, and then repeatedly as long as the specified condition evaluates to true.
+ * The general syntax of a do...while loop in JavaScript is as follows:
+ * (inlization)
+ * do {
+ *   code to be executed at least once and then repeatedly as long as the condition is true
+ *   (iteration)
+ * } while (condition);
+ */
+
+let a=1
+do{
+    console.log("Hey there")
+    a++
+}while(a<3)
