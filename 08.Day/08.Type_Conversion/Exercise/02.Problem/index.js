b8302831306622075da75074bd951fece510c163
Vignesh061 <vigneshvicky66005@gmail.com>
2025-04-30 22:23:17 +0530

    Implemented Problems Using Type Conversion

---
+++ 08.Day/08.Type_Conversion/Exercise/02.Problem/index.js
@@ -0,0 +1,26 @@
+console.log("Example 1")
+let a="Hello";
+let b="Hello";
+let c="Hello";
+
+a=Number(a);
+b=String(b);
+c=Boolean(c);
+
+console.log(a,typeof a);
+console.log(b,typeof b);
+console.log(c,typeof c);
+
+
+console.log("Example 2 \n")
+let x="0";
+let y="0";
+let z="0";
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y)
+console.log(z,typeof z)
