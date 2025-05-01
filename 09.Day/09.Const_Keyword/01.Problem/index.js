d8b86003b35be26b24fd0b414834a05db5353f97
Vignesh061 <vigneshvicky66005@gmail.com>
2025-05-01 22:14:34 +0530

    Implemented Problems Using Const Keyword

---
+++ 09.Day/09.Const_Keyword/01.Problem/index.js
@@ -0,0 +1,11 @@
+//const = a variable that cant be changed
+
+const PI=3.14159;
+let radius
+let circumferance;
+
+radius = window.prompt("Enter the radius of a cirule:")
+radius=Number(radius);
+
+circumferance=2*PI*radius;
+console.log(circumferance);
