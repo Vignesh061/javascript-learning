e379c3bb42d9c1fe1e5a6f8937112bcaeea05b75
Vignesh061 <vigneshvicky66005@gmail.com>
2025-05-03 20:40:37 +0530

    Math Build In Object

---
+++ 11.Day/11.Math/index.js
@@ -0,0 +1,23 @@
+//math = built in object that provides a collection of properties and methods
+
+let x=2.532;
+let y=4;
+let z=5;
+
+//a=Math.round(x);
+//a=Math.floor(x);
+//a=Math.ceil(x); 
+// a=Math.trunc(x);
+// a=Math.pow(y,z);
+// a=Math.sqrt(x);
+// a=Math.sin(x);
+// a=Math.cos(x);
+// a=Math.tan(x);
+// a=Math.abs(x);
+// a=Math.sign(x);
+
+// let max = Math.max(x,y,z);
+ let min= Math.min(x,y,z);
+// console.log(a);
+// console.log(max);
+ console.log(min);
