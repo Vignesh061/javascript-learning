baee2ebb4da18f0f9e49d1fdead8226c26c2c4c5
Vignesh061 <vigneshvicky66005@gmail.com>
2025-05-11 09:27:51 +0530

    Number Guessing Game

---
+++ 18.Day/19.Number_Guessing_Game/script.js
@@ -0,0 +1,40 @@
+//number guessing game
+
+const min=1;
+const max=100;
+const answer=Math.floor(Math.random()*max-min+1)+min;
+
+let attempt = 0;
+let guess;
+let running=true;
+
+while(running)
+{
+    guess=window.prompt(`Guess a numer between ${min} -${max}`)
+    guess=Number(guess);
+
+    if(isNaN(guess))
+    {
+        window.alert(`Please enter a valid number`);
+        
+    }
+    else if(guess<min || guess>max){
+        window.alert(`Please enter a valid number`);
+
+    }
+    else{
+        attempt++;
+        if(guess<answer)
+        {
+            window.alert(`TOO LOW! TRY AGAIN!`);
+        }
+        else if(guess>answer)
+        {
+            window.alert(`TOO HIGH! TRY AGAIN!`);
+        }
+        else{
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempt} attempts`)
+        }
+    }
+
+}
