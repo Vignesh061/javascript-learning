d56da266db1374b1c6c474cc7369fd0dfa6b4df8
Vignesh061 <vigneshvicky66005@gmail.com>
2025-05-17 20:44:52 +0530

    Iterate an Array

---
+++ 21.Day/22.Iterate_an_array/script.js
@@ -0,0 +1,25 @@
+let fruits=["apple", "orange","banana","mango"]
+//sort is used to arrange the elements in  alphabet order in the array;
+// output
+/**
+ * apple
+ * banana
+ * orange
+ * mango
+ */
+
+//sort().reverse();
+// It is used to arrange the elements in alphabet in reverse in the array;
+/**
+ * mango
+ * orange
+ * banana
+ * apple
+ * 
+ */
+fruits.sort().reverse()
+for(i=0;i<=fruits.length;i++)
+{
+    console.log(fruits[i]);
+}
+
