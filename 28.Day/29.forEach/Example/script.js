808aae39b730705f4ff5de9013e9769e70af4a7b
Vignesh061 <vigneshvicky66005@gmail.com>
2025-05-25 22:08:37 +0530

    forEach

---
+++ 28.Day/29.forEach/Example/script.js
@@ -0,0 +1,23 @@
+// -------------- EXAMPLE 2 --------------
+
+let fruits = ["clever", "code", "quest"];
+
+fruits.forEach(capitalize);
+fruits.forEach(display);
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
+}
+
+function lowercase(element, index, array){
+    array[index] = element.toLowerCase();
+}
+
+function capitalize(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+
+function display(element){
+    console.log(element);
+}
