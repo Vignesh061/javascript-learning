76fb68aaf9b810956c443d5e794e1fc69fb71a6b
Vignesh061 <vigneshvicky3611@gmail.com>
2025-06-17 23:37:35 +0530

    Static Keyword Syntax

---
+++ 37.Day/39.Static_Keyword/Syntax/script.js
@@ -0,0 +1,20 @@
+
+// In JavaScript, the static keyword is used inside classes to define a method or property that belongs to the class itself, not to instances (objects) created from the class.
+
+
+// Static Property Syntax
+class ClassName{
+    static propertyName = value
+
+}
+
+console.log(ClassName.propertyName);
+
+
+
+// Static Method Syntax
+class ClassNaem{
+    static methodName(){
+        // code block
+    }
+}
