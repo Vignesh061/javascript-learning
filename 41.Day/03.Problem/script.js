08a3d550fddb8a146560dcfa538de4e402bb4ad1
Vignesh061 <vigneshvicky3611@gmail.com>
2025-08-09 22:08:16 +0530

    Implemented Problem using

---
+++ 41.Day/03.Problem/script.js
@@ -0,0 +1,10 @@
+// Problem 
+
+let color = [11,12,13,14,15];
+
+[firstnumber, secondnumber,thirdnumber ,...othernumbers]=color;
+
+console.log(firstnumber)
+console.log(secondnumber)
+console.log(thirdnumber)
+console.log(othernumbers)
